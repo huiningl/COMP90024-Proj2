64803591c41065bd697a9d9b6b75497f51f25b07
Huining Li <huiningl@github.com>
2019-05-10 23:54:37 +1000

    plot: time distribution & sentiment time

--- Flask_Test/plot/figure_plot.py
+++ Flask_Test/plot/figure_plot.py
@@ -1,10 +1,12 @@
 from collections import Counter
 
 import couchdb
-import matplotlib.pyplot as plt
+import couchdb
+import matplotlib.pylab as plt
 import numpy as np
-import vincent
-from vincent import AxisProperties, PropertySet, ValueRef
+from bokeh.io import show, output_file
+from bokeh.models import ColumnDataSource, FactorRange
+from bokeh.plotting import figure
 
 
 class Plotter:
@@ -13,45 +15,115 @@ class Plotter:
         self.url = 'http://10.9.131.64:5984'
         self.couch_server = couchdb.Server(url=self.url)
 
-    def bar_plot(self, doc_id, db_name):
-        db = self.couch_server[db_name]
-        data = db.get(doc_id)
-        top10 = sorted(data['data'].itmes(), reverse=True, key=lambda x: x[-1])[:10]
-        # count_terms_only = Counter()
-        # count_terms_only.update(data['data'])
-        # top10 = count_terms_only.most_common(10)
-        #hashtag, occurrences = zip(*top10)
-        dict_top10 = dict(top10)
-        hashTags = list(dict_top10.keys())
-        occurrences = list(dict_top10.values())
-        fig = plt.figure()
-        N = 10
-        ind = np.arrange(N)
-        width = 0.2
+    def bar_plot_sentiment_time(self, doc_id, db_name):
+        # db = self.couch_server[db_name]
+        # data = db.get(doc_id)
+        couch_server = couchdb.Server(url='http://10.9.131.221:5984')
+        source_db = couch_server['test_db']
 
-        p = plt.bar(ind,occurrences,width,bottom=None)
+        data = source_db.get('sentiment_time')
+        original_data = data['data']
+        time_list = list(original_data.keys())
+        print("time_list: ", time_list)
+        print("original_data: ", original_data)
 
-        plt.ylabel('Occurences')
-        plt.xlabel('Hashtags')
-        plt.title('Hashtag Occurrence')
-        plt.xticks(ind,hashTags)
-        plt.yticks(np.arrange(0,100,10))
+        sorted_original_data = {}
+        for time, info in original_data.items():
+            sorted_item = {}
+            sorted_item['neg'] = info['neg']
+            sorted_item['neu'] = info['neu']
+            sorted_item['pos'] = info['pos']
+            sorted_original_data[time] = sorted_item
+        print("sorted_original_data", sorted_original_data)
 
-        # plt.clf()
-        # # an empty figure with no axes
-        # fig = plt.figure()
-        # for each in data['data']:
-        #     plt.plot(each['coordinates'])
+        dict_list = list(sorted_original_data.values())
+        print("dict_list: ", dict_list)
+        bar_data_dict = {}
+        bar_data_dict['time'] = time_list
+        bar_data_dict['neg'] = []
+        bar_data_dict['neu'] = []
+        bar_data_dict['pos'] = []
+        for time, info in sorted_original_data.items():
+            for k, v in info.items():
+                bar_data_dict_item = {}
+                bar_data_dict_item[k] = v
+                if k == 'neg':
+                    bar_data_dict['neg'].append(v)
+                if k == 'neu':
+                    bar_data_dict['neu'].append(v)
+                if k == 'pos':
+                    bar_data_dict['pos'].append(v)
+        print("bar_data_dict: ", bar_data_dict)
+        counts = sum(zip(bar_data_dict['neg'], bar_data_dict['neu'], bar_data_dict['pos']), ())
+        print("counts: ", counts)
 
-        plt.show()
-        fig.savefig('./static/images/figures/hashtag.png')
+        sentiment_list = list(dict_list[0].keys())
+        print(sentiment_list)
+        x = [(time, sentiment) for time in time_list for sentiment in sentiment_list]
+        print("xlabel: ", x)
+
+        source = ColumnDataSource(data=dict(x=x, counts=counts))
+
+        p = figure(x_range=FactorRange(*x), plot_height=250, title="Sentiment Time",
+                   toolbar_location=None, tools="")
+        p.vbar(x='x', top='counts', width=0.9, source=source)
+        # p.vbar(x='x', top='counts', width=0.9, source=source, line_color="white")
+        # fill_color=factor_cmap('x', palette=palette, factors=sentiment_list, start=1, end=2))
+
+        p.y_range.start = 0
+        p.x_range.range_padding = 0.1
+        p.xaxis.major_label_orientation = 1
+        p.xgrid.grid_line_color = None
+
+        show(p)
+
+    def bar_plot_time_distribution(self, doc_id, db_name):
+        couch_server = couchdb.Server(url='http://10.9.131.221:5984')
+        source_db = couch_server['test_db']
+        data = source_db.get('time_distribution')
+        original_data = data['data']
+        modified_data = original_data
+        modified_data['24'] = modified_data['0']
+        del modified_data['0']
+        print('modified_data: ', modified_data)
+
+        # TODO: Sort Key
+        # for k,v in modified_data.items():
+        #     int(k)
+        #     print(k)
+        #     k = sorted(k)
+        #
+        # print(modified_data)
+
+        time = []
+        num = []
+        for k, v in modified_data.items():
+            time.append(k)
+            num.append(v)
+        print(time)
+        print(num)
+        rate_list = []
+        suml = sum(num)
+        for n in num:
+            rate = n / suml
+            rate_list.append(rate)
+        print("rate_list: ", rate_list)
+
+        TOOLTIPS = [("time", "@x"), ("number", "@y")]
+
+        p = figure(x_range=time, plot_height=400, title="Time Distribution",
+                   toolbar_location=None, tools="hover", tooltips=TOOLTIPS)
+
+        # p.vbar(x=time, top=num, width=0.9)
+        p.line(time, num, line_width=0.9)
+
+        p.xgrid.grid_line_color = None
+        p.y_range.start = 0
+
+        show(p)
+
 
 
     def other_plot(self):
         pass
 
-
-# count_terms_only = Counter()
-# count_terms_only.update(hashtag_occurrence)
-# top10 = count_terms_only.most_common(10)
-# hashtag, occurrences = zip(*top10)
